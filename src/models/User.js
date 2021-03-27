c2e6551983c8fd2f12f6d7900156fd6da54737f4
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-27 15:04:28 +0530

    Hashing the password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const SALT_I = 10;
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,6 +12,35 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// This will be invoked before save method
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    // Will be accessed if the user's password is modified
+    if (user.isModified('password')) {
+        bcrypt.genSalt(SALT_I, (err, salt) => {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) return next(err);
+
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+// Creating the method for user to compare the password with hash
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
