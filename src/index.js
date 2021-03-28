b4ec790fc3f63c2d27f337b19ddd955e1cd2e93c
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-28 23:01:55 +0530

    Get port from env

--- src/index.js
+++ src/index.js
@@ -32,6 +32,6 @@ app.get('/', (req, res) => {
     res.send('Initial Setup done')
 });
 
-app.listen(3000, () => {
-    console.log('Server is listening on Port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is listening')
 });
